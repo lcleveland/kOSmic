ed043ed81fd3cee3c84e388bc776e5b38de6cbf9
lcleveland <lyle.cleveland@proton.me>
2024-09-21 21:49:55 -0500

    structure

---
+++ core/settings/nix/allow_unfree.nix
@@ -0,0 +1,17 @@
+#   This file defines the settings for allowing 'unfree' features in the OS.
+{ config, lib, ... }:
+let
+  cfg = config.core.settings.allow_unfree;
+in
+{
+  options.core.settings.allow_unfree = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = lib.mdDoc "Enable 'unfree' features in the OS.";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    nixpkgs.config.allowUnfree = cfg.enable;
+  };
+}
