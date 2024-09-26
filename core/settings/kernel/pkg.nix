db12177b7b0ca2b37fb09b5184480ddb374df201
lcleveland <lyle.cleveland@proton.me>
2024-09-26 13:39:43 -0500

    organize and add imports

---
+++ core/settings/kernel/pkg.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.core.settings.kernel;
+in
+{
+  options.core.settings.kernel = {
+    pkg = lib.mkOption {
+      type = lib.types.attrs;
+      default = pkgs.linuxPackages_latest;
+      description = lib.mdDoc "The kernel package to use";
+    };
+  };
+  config = {
+    boot.kernelPackages = cfg.pkg;
+  };
+}
