54569f6a6e569e20636c6b725e13284c3644596a
lcleveland <lyle.cleveland@proton.me>
2024-09-28 15:17:36 -0500

    change desc

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "A NixOS that is based on the Cosmic DE";
 
   outputs = inputs@{ home_manager, nixpkgs, nixpkgs_stable, nixvim, self }: { };
   inputs = {
