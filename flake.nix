c17ecf558c0b85cbe90e4e5ba52387431e20e3d0
lcleveland <lyle.cleveland@proton.me>
2024-10-01 08:53:48 -0500

    init flake.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,28 @@
 {
   description = "A NixOS that is based on the Cosmic DE";
 
-  outputs = inputs@{ home_manager, nixpkgs, nixpkgs_stable, nixvim, self }: { };
+  outputs = inputs@{ config, home_manager, nixpkgs, nixpkgs_stable, nixvim, self }:
+    let
+      base_modules = [
+        ./core
+        home_manager.nixosModules.home-manager
+      ];
+      make_unstable_system = profile: extra_modules:
+        nixpkgs.lib.nixosSystem {
+          system = config.core.settings.nix.system;
+          modules = base_modules ++ extra_modules;
+        };
+      make_stable_system = profile: extra_modules:
+        nixpkgs_stable.lib.nixosSystem {
+          system = config.core.settings.nix.system;
+          modules = base_modules ++ extra_modules;
+        };
+    in
+    {
+      nixosConfigurations = {
+        home = make_unstable_system;
+      };
+    };
   inputs = {
     home_manager = {
       inputs.nixpkgs.follows = "nixpkgs";
