b990ba33a88e0bb445087831952d344968885b49
lcleveland <lyle.cleveland@proton.me>
2024-10-05 19:31:44 -0500

    add settings

--- core/settings/desktop_environment/cosmic/display_manager.nix
+++ core/settings/desktop_environment/cosmic/display_manager.nix
@@ -1,16 +1,17 @@
 { config, lib, ... }:
 let
-  cfg = config.core.settings.display_manager;
+  cfg = config.core.settings.desktop_environment.cosmic.display_manager;
+  cosmic = config.core.settings.desktop_environment.cosmic;
 in
 {
-  options.core.settings.cosmic.display_manager = {
+  options.core.settings.desktop_environment.cosmic.display_manager = {
     enable = lib.mkOption {
       type = lib.types.bool;
       default = true;
       description = "Enable display manager";
     };
   };
-  config = lib.mkIf cfg.enable && config.core.settings.cosmic.enable {
+  config = lib.mkIf (cfg.enable && cosmic.enable) {
     services.displayManager.cosmic-greeter.enable = cfg.enable;
   };
 }
