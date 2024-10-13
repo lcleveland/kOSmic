c9eed9cf9ff6a6f9b292af7bd72c3f426d1b73e9
lcleveland <lyle.cleveland@proton.me>
2024-10-13 17:36:24 -0500

    add setting

--- users/template/settings/default.nix
+++ users/template/settings/default.nix
@@ -5,6 +5,7 @@
     (import ./email.nix { username = username; })
     (import ./enable.nix { username = username; })
     (import ./groups.nix { username = username; })
+    (import ./is_normal_user.nix { username = username; })
     (import ./name.nix { username = username; })
     (import ./state_version.nix { username = username; })
   ];
