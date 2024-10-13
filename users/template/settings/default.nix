02efad5eca0ff91f4a3b2269de0e4f172ef5deb6
lcleveland <lyle.cleveland@proton.me>
2024-10-13 14:55:49 -0500

    move to own mod

--- users/template/settings/default.nix
+++ users/template/settings/default.nix
@@ -2,6 +2,8 @@
 { ... }:
 {
   imports = [
+    (import ./enable.nix { username = username; })
     (import ./name.nix { username = username; })
+    (import ./state_version.nix { username = username; })
   ];
 }
---
+++ users/template/settings/state_version.nix
@@ -0,0 +1,23 @@
+{ username, ... }:
+{ config, lib, ... }:
+let
+  cfg = config.users.${username};
+in
+{
+  options.users.${username} = {
+    name = lib.mkOption {
+      type = lib.types.str;
+      default = "24.05";
+      description = "The state version for home manager for this user";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    home-manager.users.${username} = {
+      home = {
+        stateVersion = "${cfg.state_version}";
+        username = "${username}";
+      };
+    };
+
+  };
+}
