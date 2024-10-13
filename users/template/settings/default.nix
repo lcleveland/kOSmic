de6d67d6f3faa3106cd1ccfe31671dbd5e1f454d
lcleveland <lyle.cleveland@proton.me>
2024-10-13 17:21:08 -0500

    add email settings

--- users/template/settings/default.nix
+++ users/template/settings/default.nix
@@ -2,6 +2,7 @@
 { ... }:
 {
   imports = [
+    (import ./email.nix { username = username; })
     (import ./enable.nix { username = username; })
     (import ./name.nix { username = username; })
     (import ./state_version.nix { username = username; })
