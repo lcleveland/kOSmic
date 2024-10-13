b6f50decda7d2c2c51bde7fc35141c1948f28503
lcleveland <lyle.cleveland@proton.me>
2024-10-13 11:36:17 -0500

    test

--- users/template/default.nix
+++ users/template/default.nix
@@ -1,5 +1,6 @@
-{ ... }:
+{ config, lib, ... }:
 let
+  cfg = config.users.${username};
   username = "template";
 in
 {
@@ -7,4 +8,12 @@ in
     ./applications
     ./settings
   ];
+  options.users.${username} = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable the user";
+    };
+  };
+  config.users.${username} = lib.mkIf cfg.enable { };
 }
