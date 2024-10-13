796b8335af6fee6ee5e231e7ae5d55106b9f799d
lcleveland <lyle.cleveland@proton.me>
2024-10-13 14:59:44 -0500

    fix

--- users/template/default.nix
+++ users/template/default.nix
@@ -1,6 +1,5 @@
-{ config, lib, ... }:
+{ ... }:
 let
-  cfg = config.users.${username};
   username = "template";
 in
 {
@@ -8,24 +7,4 @@ in
     ./applications
     (import ./settings { username = username; })
   ];
-  options.users.${username} = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Enable the user";
-    };
-    state_version = lib.mkOption {
-      type = lib.types.str;
-      default = "24.05";
-      description = "The state version for home manager for this user";
-    };
-  };
-  config = lib.mkIf cfg.enable {
-    home-manager.users.${username} = {
-      home = {
-        stateVersion = "${cfg.state_version}";
-        username = "${username}";
-      };
-    };
-  };
 }
