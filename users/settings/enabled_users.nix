737858001a6af8de2293d7b4b89eab67670e6a28
lcleveland <lyle.cleveland@proton.me>
2024-10-14 09:53:13 -0500

    test

--- users/settings/enabled_users.nix
+++ users/settings/enabled_users.nix
@@ -1,1 +1,16 @@
-{ ... }: { }
+{ config, lib, ... }:
+let
+  cfg = config.users.settings;
+in
+{
+  options.users.settings = {
+    enabled_users = {
+      type = lib.types.listOf lib.types.str;
+      default = [ "template" ];
+      description = "List of enabled users";
+    };
+  };
+  config = {
+    enable_map = map (user: { config.users.${user}.enable = true; }) cfg.enabled_users;
+  };
+}
