360a37c086f5bb61eb090e39aa871ad0b3bd27a5
lcleveland <lyle.cleveland@proton.me>
2024-11-08 08:48:21 -0600

    grub fix

--- core/profiles/test_vm.nix
+++ core/profiles/test_vm.nix
@@ -4,5 +4,8 @@
   ];
   users.lcleveland.enable = true;
   core.settings.boot.systemd.enable = false;
-  boot.loader.grub.enable = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/vda1";
+  };
 }
